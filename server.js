3e6b7a5265c1dbb4bda481ddc5a4198315579f23
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-15 16:42:18 -0500

    Updated to sever port 3001, set up POST route for campgrounds

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+const campgrounds = [
+  { name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg' },
+  { name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg' },
+  { name: 'Mountain Goats Rest', image: 'https://farm9.staticflickr.com/8454/7930198240_856a39bf27.jpg' }
+];
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    { name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg' },
-    { name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg' },
-    { name: 'Mountain Goats Rest', image: 'https://farm9.staticflickr.com/8454/7930198240_856a39bf27.jpg' }
-  ];
+
   res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.get('/campgrounds/create', (req, res) => {
+    res.render('create');
+});
+
+app.post('/campgrounds', (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect('campgrounds')
 });
 
 app.listen(PORT, () => {
