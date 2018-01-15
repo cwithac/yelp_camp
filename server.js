219a4b71d8f0ef99d3a38fd9819cb9e82643d516
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-15 15:53:23 -0500

    Initial root route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Testing')
+});
 
 app.listen(PORT, () => {
   console.log('Yelp Camp is listening on port', PORT);
