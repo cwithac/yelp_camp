72913e767ebf94bbc3e1ce64261b166158611fb9
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-15 16:04:35 -0500

    /camgrounds templating

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/campgrounds', (req, res) => {
+  const campgrounds = [
+    { name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg' },
+    { name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg' },
+    { name: 'Mountain Goats Rest', image: 'https://farm9.staticflickr.com/8454/7930198240_856a39bf27.jpg' }
+  ];
+  res.render('campgrounds')
+});
+
 app.listen(PORT, () => {
   console.log('Yelp Camp is listening on port', PORT);
 });
