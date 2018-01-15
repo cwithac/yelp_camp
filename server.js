f247a48f4bc4776d7153f82597652d9786f2d47f
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-15 17:16:25 -0500

    Update bootstrap CDN version, add css to public, basic bootstrap styling to /campgrounds

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(express.json());
 const campgrounds = [
   { name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg' },
   { name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg' },
+  { name: 'Mountain Goats Rest', image: 'https://farm9.staticflickr.com/8454/7930198240_856a39bf27.jpg' },
+  { name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg' },
+  { name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg' },
+  { name: 'Mountain Goats Rest', image: 'https://farm9.staticflickr.com/8454/7930198240_856a39bf27.jpg' },
+  { name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg' },
+  { name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg' },
   { name: 'Mountain Goats Rest', image: 'https://farm9.staticflickr.com/8454/7930198240_856a39bf27.jpg' }
 ];
 
