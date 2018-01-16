05c49a121bf5afd076fe921afce0a271d6b2da31
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-16 16:21:07 -0500

    Redirect after comment creation modified

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,7 +7,7 @@ commentsRouter.post('/', async (req, res) => {
   try {
     req.body.content = req.sanitize(req.body.content);
     const newComment = await Comment.create(req.body);
-    res.redirect('campgrounds');
+    res.redirect('/campgrounds/' + newComment.campground);
   } catch (err) {
     res.send(err.message);
   };
