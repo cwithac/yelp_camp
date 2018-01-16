26c8f81d29c2bf5796f80a75c88353a88caf1559
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-16 08:58:10 -0500

    Code commenting

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
+//Global Variables/Initiators
 const express = require('express');
 const app = express();
 const PORT = 3001;
 const mongoose = require('mongoose');
 
+//Middleware
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//Controller Middleware
 const campgroundsController = require('./controllers/campgrounds.js');
 app.use('/campgrounds', campgroundsController);
 
+//Landing Page Route
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+//Listeners for app and MongoDB
 const mongoURI = 'mongodb://localhost/yelpcamp';
 mongoose.connect(mongoURI, { useMongoClient: true});
 mongoose.Promise = global.Promise;
