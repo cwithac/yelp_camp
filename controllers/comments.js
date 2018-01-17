26d1de4d28ed12a2cb23855dd5a65cd45c335fb6
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-17 16:27:16 -0500

    Comments show associated username

--- controllers/comments.js
+++ controllers/comments.js
@@ -8,6 +8,9 @@ commentsRouter.post('/', async (req, res) => {
   try {
     req.body.content = req.sanitize(req.body.content);
     const newComment = await Comment.create(req.body);
+    newComment.author.id = req.user._id;
+    newComment.author.username = req.user.username;
+    newComment.save();
     res.redirect('/campgrounds/' + newComment.campground);
   } catch (err) {
     res.send(err.message);
--- models/comments.js
+++ models/comments.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
   content: String,
-  campground: { type: mongoose.Schema.ObjectId, ref: 'Campground'}
+  campground: { type: mongoose.Schema.ObjectId, ref: 'Campground'},
+  author: {
+            id: { type: mongoose.Schema.ObjectId, ref: 'User'},
+            username: String
+        }
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
