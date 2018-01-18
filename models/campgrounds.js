33aca7157dcb36cb0e7e6a2aa34d2b551f7c6aa7
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-18 16:23:43 -0500

    URL link and comment glyphicon change

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -6,6 +6,7 @@ const campgroundSchema = new mongoose.Schema({
   description: {type: String, default: "Just pretend you are a whisper floating across a mountain. This painting comes right out of your heart. You don't want to kill all your dark areas they are very important. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. We need dark in order to show light."},
   price: String,
   location: String,
+  locationURL: String,
   author: {
             id: { type: mongoose.Schema.ObjectId, ref: 'User'},
             username: String
