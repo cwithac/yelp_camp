8c3e3aa361f66f505a33a8a29cf0e17563fe83b5
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-23 09:30:56 -0500

    Star rating added to comments, cleanup of JS from head

--- models/comments.js
+++ models/comments.js
@@ -4,6 +4,7 @@ const commentSchema = new mongoose.Schema({
   content: String,
   campground: { type: mongoose.Schema.ObjectId, ref: 'Campground'},
   createdAt: { type: Date, default: Date.now },
+  rating: Number,
   author: {
             id: { type: mongoose.Schema.ObjectId, ref: 'User'},
             username: String
